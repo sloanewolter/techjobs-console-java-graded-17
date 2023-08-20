2692217922fa2f958fbb2a7df573434e03d28daa
Sloane Wolter <sloanewolter@gmail.com>
2023-08-20 14:53:12 -0500

    got printjobs working except for no results

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -10,7 +10,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -112,14 +112,22 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+            for (HashMap<String, String> eachJob : someJobs) {
+                System.out.println("\n" + "*****");
+                for (Map.Entry<String, String> job : eachJob.entrySet()) {
+                    System.out.println(job.getKey() + ": " + job.getValue());
 
-        System.out.println("printJobs is not implemented yet");
+                }
+                System.out.println("*****");
+            }
+        }
     }
-}
+
+
